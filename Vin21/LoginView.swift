59948178f0070660a52dc3e91011a84cccf62be6
Etienne Vautherin <etienne@vautherin.com>
2021-02-09 10:35:09 +0100

    Commentaires sur LoginView

--- Vin21/LoginView.swift
+++ Vin21/LoginView.swift
@@ -13,6 +13,7 @@ import Firebase
 struct LoginView: View {
     @ObservedObject var model: Model
     
+    // Ces 2 états sont en quelque sorte le ViewModel de LoginView
     @State var email = "etienne@vautherin.com"
     @State var password = "Vin21-2021"
 
@@ -23,19 +24,28 @@ struct LoginView: View {
                 SecureField("Password", text: $password)          .textFieldStyle(RoundedBorderTextFieldStyle())
             }
 
+            // Le bouton Login essaie de se connecter à Firebase avec le contenu des variables email et password lorsqu'il est pressé
             Button("Login") {
                 Auth.auth().signIn(
                     withEmail: email,
                     password: password
                 ) { (authResult, error) in
+                    // La fonction signIn appelle ce bloc d'instruction avec le résulat de l'authentification
+                    // error et authResut sont des optionnels
+                    
+                    // Si error est défini, probème d'authentification
+                    // Sinon authentification OK
                     if let error = error {
                         print("Authentification error: \(error.localizedDescription)")
                     } else {
                         print("No authentification error")
                     }
                     
+                    // Si authResult est défini, sa propriété user est recopiée dans model
                     model.user = authResult?.user
-                    
+    
+                    // Autre manière d'écrire l'instruction précédente
+                    // Plus générale mais plus "lourde"
     //                    if let authResult = authResult {
     //                        model.user = authResult.user
     //                    }
